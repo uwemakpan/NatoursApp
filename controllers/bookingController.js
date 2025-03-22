e7d654aafce5705490fa39bd3eb8fa9aa2b86af5
akpanu <akpanmichael90@gmail.com>
2025-03-22 15:00:51 +0100

    Improved Stripe Implementation4

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -79,7 +79,8 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
 
-  const price = session.line_items.price_data.unit_amount_decimal / 100;
+  const price = line_items.price_data.unit_amount;
+
   // const price = 100;
   await Booking.create({ tour, user, price });
 };
