ff7d75d51c8431df754380538b32facbc342f6ba
akpanu <akpanmichael90@gmail.com>
2025-03-22 12:02:05 +0100

    Improved Stripe Implementation4

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -78,7 +78,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   // const price = session.display_items[0].amount / 100;
-  const price = session.line_items.unit_amount / 100;
+  const price = session.line_items[0].price_data.unit_amount / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -96,7 +96,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
   if (event.type === 'checkout.session.completed') {
-    createBookingCheckout(event.object);
+    createBookingCheckout(event.data.object);
     // createBookingCheckout(event.data.object);
   }
   res.status(200).json({ received: true });
